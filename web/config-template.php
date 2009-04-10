a8ac61701cb67cf4ec4b6acfa020b78573f9ac59
harry <harry>
2009-04-10 10:26:11 +0000

    Initial import

---
+++ web/config-template.php
@@ -0,0 +1,15 @@
+<?php
+
+//
+// MySQL Configuration
+//
+
+$config['mySQL']['server'] = 'localhost';
+$config['mySQL']['username'] = 'root';
+$config['mySQL']['password'] = '';
+$config['mySQL']['database'] = 'scenic';
+
+$config['site']['image_sysdir'] = '/absolute/path/to/img/dir';
+$config['site']['image_webdir'] = '/webroot/path/to/img/dir';
+
+?>
---
+++ web/index.php
@@ -0,0 +1,24 @@
+<?php
+
+include ROOT . '/include/global.php';
+
+$route = sanitise($_REQUEST, array(
+   'action' => array(ENUM, array('home', 'rate', 'about', 'top10', 'view', 'rankings')),
+   'format' => array(ENUM, array('html')),
+), array(
+   'action' => 'home',
+   'format' => 'html',   
+));
+
+
+// They're not allowed to unset format on purpose -- set it here, even if the query string has it empty
+if(!$route['format'])
+{
+   $route['format'] = 'html';
+}
+
+define('VIEW_DIR', ROOT . "/view/{$route['format']}");
+
+include ROOT . "/app/{$route['action']}.php";
+
+?>
