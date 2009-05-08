0bb61f3e75a876d5a69915dea7f8d248e45cb745
matthew <matthew>
2009-05-08 11:22:12 +0000

    Bugfixes, add to cron.

--- bin/generate-tsv.php
+++ bin/generate-tsv.php
@@ -7,7 +7,7 @@
  * Copyright (c) 2009 UK Citizens Online Democracy. All rights reserved.
  * Email: matthew@mysociety.org. WWW: http://www.mysociety.org/
  *
- * $Id: generate-tsv.php,v 1.1 2009-05-08 11:12:52 matthew Exp $
+ * $Id: generate-tsv.php,v 1.2 2009-05-08 11:22:12 matthew Exp $
  *
  */
 
@@ -19,16 +19,14 @@ $mySQL->query("select place, lat, lon, avg(rating) from vote, place
         and uuid not in ('2a92cf77c3908f132b856af79f0c3267', 'fd0fd60993feaa3463c43581ee60eeba', 'd2947b36351a635e7381a17a5b22b0ba')
     group by place
     having count(rating) >= 3
-    into outfile '/tmp/Votes20090508-1144'
 ");
 
 $out = '';
 while ($row = $mySQL->fetchArray()) {
-    $out .= join("\t", $row) . "\n";
+    $out .= "$row[0]\t$row[1]\t$row[2]\t$row[3]\n";
 }
 
-
-$fp = fopen(ROOT . '/votes.tsv.new');
+$fp = fopen(ROOT . '/votes.tsv.new', 'w');
 fwrite($fp, $out);
 fclose($fp);
 rename(ROOT . '/votes.tsv.new', ROOT . '/votes.tsv');
