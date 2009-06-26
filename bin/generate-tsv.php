095143cc533182b5d1e03566c474c12ccffd2d07
matthew <matthew>
2009-06-26 08:25:15 +0000

    Include votes.

--- bin/generate-tsv.php
+++ bin/generate-tsv.php
@@ -7,14 +7,14 @@
  * Copyright (c) 2009 UK Citizens Online Democracy. All rights reserved.
  * Email: matthew@mysociety.org. WWW: http://www.mysociety.org/
  *
- * $Id: generate-tsv.php,v 1.2 2009-05-08 11:22:12 matthew Exp $
+ * $Id: generate-tsv.php,v 1.3 2009-06-26 08:25:15 matthew Exp $
  *
  */
 
 include "../web/prepend.php";
 include ROOT . "/include/global.php";
 
-$mySQL->query("select place, lat, lon, avg(rating) from vote, place
+$mySQL->query("select place, lat, lon, avg(rating), group_concat(rating) from vote, place
     where place = place.id
         and uuid not in ('2a92cf77c3908f132b856af79f0c3267', 'fd0fd60993feaa3463c43581ee60eeba', 'd2947b36351a635e7381a17a5b22b0ba')
     group by place
@@ -23,7 +23,7 @@ $mySQL->query("select place, lat, lon, avg(rating) from vote, place
 
 $out = '';
 while ($row = $mySQL->fetchArray()) {
-    $out .= "$row[0]\t$row[1]\t$row[2]\t$row[3]\n";
+    $out .= join("\t", $row) . "\n";
 }
 
 $fp = fopen(ROOT . '/votes.tsv.new', 'w');
