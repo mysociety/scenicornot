3ab33b62b89f1d5d74c3b8a77d4836d76498c303
matthew <matthew>
2009-08-10 08:53:51 +0000

    Make bug email a config variable

--- web/app/error.php
+++ web/app/error.php
@@ -18,10 +18,10 @@ if(!isset($heading))
 
 if(!isset($error))
 {
-   $error = "Something went wrong. If this was unexpected, please <a href='mailto:team@mysociety.org'>let us know</a>!";
+   $error = "Something went wrong. If this was unexpected, please <a href='mailto:team&#64;mysociety.org'>let us know</a>!";
 }
 
-mail('harry@thedextrousweb.com', 'ScenicOrNot error report', "http://$_SERVER[HTTP_HOST]$_SERVER[PHP_SELF]?$_SERVER[QUERY_STRING]\n$title\n$heading\n$error\n\nRequest: " . print_r($_REQUEST, true) . "\nMySQL: " . mysql_error() . "\nServer: " . print_r($_SERVER, true));
+mail(OPTION_BUGS_EMAIL, 'ScenicOrNot error report', "http://$_SERVER[HTTP_HOST]$_SERVER[PHP_SELF]?$_SERVER[QUERY_STRING]\n$title\n$heading\n$error\n\nRequest: " . print_r($_REQUEST, true) . "\nMySQL: " . mysql_error() . "\nServer: " . print_r($_SERVER, true));
 
 include VIEW_DIR . "/_error.php";
 
