c02196da2c1b1651890afc993c199c6aa240b91a
matthew <matthew>
2011-01-17 22:39:42 +0000

    Note about making sure no blank HTML lines in config.

--- web/config-template.php
+++ web/config-template.php
@@ -15,4 +15,5 @@ $config['site']['image_webdir'] = '/webroot/path/to/img/dir';
 $config['site']['secret']       = 'put a nice long random string here';
 
 $config['site']['place_count'] = 217674;
-?>
+
+# Deliberate no end of PHP here so that no blank lines creep in.
