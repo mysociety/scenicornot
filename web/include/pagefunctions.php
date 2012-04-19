65915c4d1a24072ee8a715ba045b237d15d8751a
Louise Crow <louise.crow@gmail.com>
2012-04-19 17:54:11 +0100

    Fix js.

--- web/include/pagefunctions.php
+++ web/include/pagefunctions.php
@@ -42,6 +42,20 @@ function head($title, $styles = array(), $javascripts = array(), $head_tags = ar
       }
 
       ?>
+      <script type="text/javascript">
+
+        var _gaq = _gaq || [];
+        _gaq.push(['_setAccount', 'UA-660910-9']);
+        _gaq.push(['_gat._anonymizeIp']);
+        _gaq.push(['_trackPageview']);
+
+        (function() {
+          var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+          ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+          var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+        })();
+
+      </script>
    </head>
 
    <body>
@@ -77,20 +91,6 @@ function foot()
          </p>
       </div>
    </div>
-   <script type="text/javascript">
-
-     var _gaq = _gaq || [];
-     _gaq.push(['_setAccount', 'UA-660910-9']);
-     _gaq.push (['_gat._anonymizeIp']);
-     _gaq.push(['_trackPageview']);
-
-     (function() {
-       var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-       ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-       var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-     })();
-
-   </script>
    </body>
    </html>
    <?
